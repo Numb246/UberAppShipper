cc6ec8ee26ded03bfa4bd22d1395467c3af33ca7
HuynhVoViet <16521436@gm.uit.edu.vn>
2022-06-03 22:26:44 +0700

    order item, implemmenting bottom sheep, order details
    done

---
+++ UberAppShipper/src/screens/OrdersScreen/index.js
@@ -0,0 +1,29 @@
+import { useRef, useMemo } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import orders from '../../../assets/data/orders.json'
+import OrderItem from '../../components/OrderItem';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+const OrdersScreen = () => {
+    const bottomSheetRef = useRef(null);
+
+    const snapPoints =useMemo(() => ["12%", "95%"], [] )
+    return (
+    <GestureHandlerRootView  style={{backgroundColor: "lightblue", flex: 1}}>    
+        <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints}>
+            <View style={{alignItems: "center", marginBottom: 30 }}>
+                <Text style={{fontSize: 20, fontWeight: '600',letterSpacing: 0.5, paddingBottom: 5}}>You're Online</Text>
+                <Text style={{letterSpacing: 0.5, color: "grey"}}>Available Orders: {orders.length}</Text>
+            </View>
+            <FlatList
+                    data={orders}
+                    renderItem={({ item }) => <OrderItem order={item} />}
+            />
+        </BottomSheet>
+
+    </GestureHandlerRootView>    
+    );
+};
+
+export default OrdersScreen;
